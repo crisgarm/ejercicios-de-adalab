70d03ce61d1a7ec5f7491bd038c88be2e3d98336
crisgarm <crisgarm9@gmail.com>
2020-10-01 19:00:54 +0200

    Add exercise 5 about hours lived

---
+++ Modulo-02/modulo-02-leccion-02-ejercicio-05/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Ejercicio 05
+
+const hoursDay = 24;
+const daysYear = 365;
+const yearsCristina = 27;
+
+const hoursYear = hoursDay * daysYear;
+console.log(hoursYear);
+
+const hoursLived = hoursYear * yearsCristina;
+console.log(hoursLived);
