ea203eb8b276e54a0fb56e1fe9c47b3ba615842c
crisgarm <crisgarm9@gmail.com>
2020-10-01 20:09:14 +0200

    Add exercise 09 about length

---
+++ Modulo-02/modulo-02-leccion-02-ejercicio-09/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Ejercicio 09
+
+const name = "Beatriz García Martín";
+console.log(
+  "El nombre de mi compañera es " +
+    name +
+    ", y está compuesto por " +
+    name.length +
+    " caracteres"
+);
