2d5d665f2a335a6d25838cb30078295e9db20d2b
crisgarm <crisgarm9@gmail.com>
2020-10-04 10:54:18 +0200

    Add exercise about access control

---
+++ Modulo-02/modulo-02-leccion-03-ejercicio-02/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Cristina";
+if (name === "Cristina") {
+  console.log("Bienvenida, Cristina.");
+} else {
+  console.log(
+    "Lo siento pero el usuario que has introducido no está registrado."
+  );
+}
