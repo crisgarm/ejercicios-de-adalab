aa8dfe09973d3702408b34e1c7c37daefd27dd98
crisgarm <crisgarm9@gmail.com>
2020-10-04 11:52:33 +0200

    Add recipe exercise

---
+++ Modulo-02/modulo-02-leccion-03-ejercicio-07/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let ingredient = "Espinacas";
+
+switch (ingredient) {
+  case "Pollo":
+    console.log(
+      "Tu ingrediente es pollo. Puedes freirte un filete con patatas."
+    );
+    break;
+  case "Merluza":
+    console.log(
+      "Tu ingrediente es merluza. Puedes hacerte una merluzita en salsa verde."
+    );
+    break;
+  case "Espinacas":
+    console.log(
+      "Tu ingrediente es espinacas. Puedes hacerte espinacas rehogadas."
+    );
+    break;
+  default:
+    console.log("Nada en la nevera.");
+}
