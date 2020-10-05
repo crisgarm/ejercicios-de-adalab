57535512e0a77ce2ea0442b81483dc348301cabf
crisgarm <crisgarm9@gmail.com>
2020-10-05 17:37:17 +0200

    Add modifications

--- Modulo-02/modulo-02-leccion-04-ejercicio-04/main.js
+++ Modulo-02/modulo-02-leccion-04-ejercicio-04/main.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const number = 345;
-
 function oddEven(number) {
-  return number;
+  if (number % 2 === 0) {
+    return true;
+    // console.log(`El número ${number} es par.`);
+  } else {
+    return false;
+    // console.log(`El número ${number} es impar.`);
+  }
 }
-if (number % 2 === 0) {
-  console.log(`El número ${number} es par.`);
-} else {
-  console.log(`El número ${number} es impar.`);
-}
+
+oddEven(397);
