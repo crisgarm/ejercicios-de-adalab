c93bee4ec9f5e020cfdf16a926c57a30359e350d
crisgarm <crisgarm9@gmail.com>
2020-10-05 22:12:16 +0200

    Add exercise 08 about next leap year

--- Modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
+++ Modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
@@ -1,4 +1,22 @@
 "use strict";
 
-let = 2020;
+function nextLeapYearCalculator(currentYear) {
+  let nextLeapYear = 0;
+  if (currentYear % 4 === 0) {
+    nextLeapYear = currentYear;
+  } else if (currentYear % 4 === 1) {
+    nextLeapYear = currentYear + 3;
+  } else if (currentYear % 4 === 2) {
+    nextLeapYear = currentYear + 2;
+  } else if (currentYear % 4 === 3) {
+    nextLeapYear = currentYear + 1;
+  }
 
+  return nextLeapYear;
+}
+
+console.log(nextLeapYearCalculator(2020));
+console.log(nextLeapYearCalculator(2021));
+console.log(nextLeapYearCalculator(2025));
+console.log(nextLeapYearCalculator(2032));
+console.log(nextLeapYearCalculator(3));
