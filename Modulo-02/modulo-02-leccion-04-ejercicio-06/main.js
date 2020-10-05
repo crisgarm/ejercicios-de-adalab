7d970d19f5f59f6d7c1003760857620ce625e759
crisgarm <crisgarm9@gmail.com>
2020-10-05 21:09:54 +0200

    Add modifications in exercise 06

--- Modulo-02/modulo-02-leccion-04-ejercicio-06/main.js
+++ Modulo-02/modulo-02-leccion-04-ejercicio-06/main.js
@@ -2,15 +2,14 @@
 
 function getEl(selector) {
   const element = document.querySelector(selector);
-  return element;
+  if (element !== null) {
+    console.log(element);
+  } else {
+    console.error(
+      `No existe ningún elemento con clase, id o tag llamado ${selector}`
+    );
+    return element;
+  }
 }
-if (element === true) {
-  console.log(element);
-} else {
-  console.error(
-    `No existe ningún elemento con clase, id o tag llamado ${selector}`
-  );
-}
-
 const btnEl = getEl(".btn");
 console.log(btnEl);
