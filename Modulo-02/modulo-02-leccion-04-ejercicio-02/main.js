eab3cc8d6a85de382a3a54090b12886824a1f76b
crisgarm <crisgarm9@gmail.com>
2020-10-05 16:43:30 +0200

    Calculate the average value

---
+++ Modulo-02/modulo-02-leccion-04-ejercicio-02/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function average(a, b, c, d) {
+  const result = (a + b + c + d) / 4;
+  return result;
+}
+const averageResult = average(5, 9, 6, 8);
+console.log(averageResult);
+
+const test02 = average(8, 19, 44, 32);
+console.log(test02);
+
+const test03 = average(0, 1, -4, 22);
+console.log(test03);
