4fd629fc02d03daf487d603ab223d14daa272166
crisgarm <crisgarm9@gmail.com>
2020-10-05 17:14:37 +0200

    Add exercise about odd or even number

---
+++ Modulo-02/modulo-02-leccion-04-ejercicio-04/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const number = 345;
+
+function oddEven(number) {
+  return number;
+}
+if (number % 2 === 0) {
+  console.log(`El número ${number} es par.`);
+} else {
+  console.log(`El número ${number} es impar.`);
+}
