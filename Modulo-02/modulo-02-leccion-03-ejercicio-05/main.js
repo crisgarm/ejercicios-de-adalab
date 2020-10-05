c6f501c5cb9377028420b2141efbb1a86263e0dc
crisgarm <crisgarm9@gmail.com>
2020-10-05 11:43:06 +0200

    Add rainbow's exercise resolved in pair programming

--- Modulo-02/modulo-02-leccion-03-ejercicio-05/main.js
+++ Modulo-02/modulo-02-leccion-03-ejercicio-05/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const containerElement = document.querySelector(".container");
+const titleElement = document.querySelector(".container__title");
+const contentElement = document.querySelector(".container__content");
+
+if (containerElement.classList.contains("warning")) {
+  titleElement.innerHTML = "Aviso";
+  contentElement.innerHTML = "Tenga cuidado";
+} else if (containerElement.classList.contains("error")) {
+  titleElement.innerHTML = "Error";
+  contentElement.innerHTML = "Ha surgido un error";
+} else if (containerElement.classList.contains("success")) {
+  titleElement.innerHTML = "Correcto";
+  contentElement.innerHTML = "Los datos son correctos";
+}
