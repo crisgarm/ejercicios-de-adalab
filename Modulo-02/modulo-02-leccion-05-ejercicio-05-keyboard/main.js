676b72f29478ec945d6989b835b83c100cebda4e
crisgarm <crisgarm9@gmail.com>
2020-10-07 13:26:04 +0200

    Add modifications in pair programming

--- Modulo-02/modulo-02-leccion-05-ejercicio-05-keyboard/main.js
+++ Modulo-02/modulo-02-leccion-05-ejercicio-05-keyboard/main.js
@@ -3,8 +3,11 @@
 const documentElement = document.querySelector(".page");
 
 function pressKeyboard(event) {
-  if (event.key === "r") documentElement.classList.add("color01");
-  else if (event.key === "p") {
+  if (event.key === "r") {
+    documentElement.classList.remove("color02");
+    documentElement.classList.add("color01");
+  } else if (event.key === "p") {
+    documentElement.classList.remove("color01");
     documentElement.classList.add("color02");
   }
 }
