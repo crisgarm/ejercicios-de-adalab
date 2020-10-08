5134d027a71973b1f5d04aff17fe50626de9f644
crisgarm <crisgarm9@gmail.com>
2020-10-08 19:46:49 +0200

    Add exercise 04

---
+++ Modulo-02/modulo-02-leccion-07-arrays.loops/modulo-02-leccion-07-ejercicio-04-moon/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+for (let i = 2020; i <= 2062; i = i + 3) {
+  console.log("Las próximas lunas serán en el año " + i);
+}
+
+// for (let i = 2020; i <= i + 3 * 15; i = i + 3) {
+//   console.log("Las próximas lunas serán en el año " + i);
+// }
