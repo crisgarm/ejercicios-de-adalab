04b4c221af831db279ebf13a28382a8e828616ab
crisgarm <crisgarm9@gmail.com>
2020-10-08 20:32:07 +0200

    Add exercise 05

---
+++ Modulo-02/modulo-02-leccion-07-arrays.loops/modulo-02-leccion-07-ejercicio-05-media/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//Recorremos el array mediante un bucle para hacer la media
+const numbers = [10, 33, 4, 15, 1];
+let acc = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  acc += numbers[i]; // acc += numbers[i] es igual a acc = acc + numbers[i]
+  console.log(acc);
+}
+acc = acc / 5;
+console.log("La puntuación final es " + acc);
