29d4d62e8b0efdbe197a84ed873d23616b5d9172
crisgarm <crisgarm9@gmail.com>
2020-10-08 18:48:54 +0200

    Add exercise 01 about arrays

---
+++ Modulo-02/modulo-02-leccion-07-arrays.loops/modulo-02-leccion-07-ejercicio-01-films/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function workWithMovies() {
+  const movies = ["Kill Bill", "Harry Potter", "El señor de los anillos"];
+  console.log(movies);
+  movies[3] = "Gladiator";
+  console.log(movies);
+  movies[1] = "Shutter Island";
+  console.log(movies);
+}
+
+workWithMovies();
