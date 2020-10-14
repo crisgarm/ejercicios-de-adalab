2280f3166b7069c232f8f423f70e96029079cb1d
crisgarm <crisgarm9@gmail.com>
2020-10-14 19:24:37 +0200

    Add exercise 01

--- Modulo-02/modulo-02-leccion-05-events/modulo-02-leccion-05-ejercicio-03-paragraph/main.js
+++ Modulo-02/modulo-02-leccion-05-events/modulo-02-leccion-05-ejercicio-03-paragraph/main.js
@@ -1,26 +1,28 @@
 "use strict";
 
 
-const createElement = document.querySelector("body");
+const createElement = document.querySelector("main");
 const content = "<p>Lorem ipsum</p>";
 const paragraph = document.querySelector(".text");
 
 
 function addParagraph(){
   createElement.innerHTML = createElement.innerHTML + content;
+  console.log(createElement);
 }
 
-// //elemento de HTML
+paragraph.addEventListener("click", addParagraph);
+
+// elemento de HTML
 // const paragraph = document.querySelector(".text");
 // const text = "<p>Lorem ipsum</p>";
 
-// //handler
+// handler
 
 // function addParagraph() {
 // let secondParagraph = document.createElement("p");
 // secondParagraph.innerHTML = text;
 // document.body.insertBefore(secondParagraph, paragraph.nextSibling);
-// // }
+// }
 
 //listener sobre el elemento, con tipo de evento y handler
-paragraph.addEventListener("mouseover", addParagraph);
