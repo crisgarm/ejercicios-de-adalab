a928ab41e90c6d3e7e5607429fa55cd0d2cfcdcc
crisgarm <crisgarm9@gmail.com>
2020-10-14 20:45:53 +0200

    Add initial commit exercise 03

---
+++ Modulo-02/modulo-02-leccion-09-arrays.loops2/modulo-02-leccion-9-ejercicio-03-concat/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+const arr = [];
+
+function bestLostNumber(){
+for (let i = 0; i < lostNumbers.lenght -1 ; i++){
+  console.log("hola");
+  if (lostNumbers[i] % 2 === 0){
+    console.log("hola");
+    arr += lostNumbers[i];
+    }
+}
+}
+bestLostNumber();
+// console.log(lostNumbers);
