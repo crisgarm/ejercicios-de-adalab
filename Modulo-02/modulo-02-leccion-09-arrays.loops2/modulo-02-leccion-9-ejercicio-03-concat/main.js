5cf39cb3f5d544fb8d787045d964dbc160cb51ce
crisgarm <crisgarm9@gmail.com>
2020-10-15 17:58:40 +0200

    Initial commit

--- Modulo-02/modulo-02-leccion-09-arrays.loops2/modulo-02-leccion-9-ejercicio-03-concat/main.js
+++ Modulo-02/modulo-02-leccion-09-arrays.loops2/modulo-02-leccion-9-ejercicio-03-concat/main.js
@@ -1,16 +1,37 @@
 "use strict";
 
-const lostNumbers = [4, 8, 15, 16, 23, 42];
-const arr = [];
+// const lostNumbers = [4, 8, 15, 16, 23, 42];
+// const arr = [];
 
-function bestLostNumber(){
-for (let i = 0; i < lostNumbers.lenght -1 ; i++){
-  console.log("hola");
-  if (lostNumbers[i] % 2 === 0){
-    console.log("hola");
-    arr += lostNumbers[i];
-    }
-}
-}
-bestLostNumber();
+// function bestLostNumber(){
+// for (let i = 0; i < lostNumbers.lenght; i++){
+//   console.log("hola");
+//   if (lostNumbers[i] % 2 === 0){
+//     console.log("hola");
+//     arr += lostNumbers[i];
+//     }
+// }
+// }
+// bestLostNumber();
 // console.log(lostNumbers);
+
+//Ejercicio en clase:
+ const lostNumbers = [4, 8, 15, 16, 23, 42];
+ const even = [];
+ const mult = [];
+
+ function bestLostNumber(){
+   for (let i = 0; i < lostNumbers.length; i++){
+     if (lostNumbers[i] % 2 === 0){
+        even.push(lostNumbers[i]);
+      }
+     if (lostNumbers[i] % 3 === 0){
+        mult.push(lostNumbers[i]);
+      }
+    }
+    console.log(even.concat(mult));
+  }
+
+ bestLostNumber();
+ console.log(even);
+ console.log(mult);
