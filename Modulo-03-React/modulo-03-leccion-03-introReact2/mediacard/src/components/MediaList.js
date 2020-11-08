0dfa61e4d278f811c3050e98b024c0c6a301eab5
crisgarm <crisgarm9@gmail.com>
2020-11-08 15:25:48 +0100

    Fix some heights

--- Modulo-03-React/modulo-03-leccion-03-introReact2/mediacard/src/components/MediaList.js
+++ Modulo-03-React/modulo-03-leccion-03-introReact2/mediacard/src/components/MediaList.js
@@ -9,7 +9,7 @@ class MediaList extends React.Component {
           <li className="list-items">
             <MediaCard
               name="Cristina García"
-              date=" Domingo 01 de noviembre deh30"
+              date=" Domingo 01 de noviembre de 2020"
               content=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit quo
               ducimus facilis libero, natus maxime dolorem voluptatem officiis.
               Fugit laudantium sed architecto commodi eum quae maiores corrupti
@@ -23,7 +23,7 @@ class MediaList extends React.Component {
           <li className="list-items">
             <MediaCard
               name="Víctor Álvarez"
-              date="Sábadoh34 de octubre deh30"
+              date="Sábado 24 de octubre de 2020"
               content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
               likes="37"
             />
@@ -31,7 +31,7 @@ class MediaList extends React.Component {
           <li className="list-items">
             <MediaCard
               name="María Romero"
-              date="Miércoles h3 de septiembre deh30"
+              date="Miércoles 02 de septiembre de 2020"
               content="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
               likes="37"
             />
