af9701032da1cddc01f1fc40936e445777171901
crisgarm <crisgarm9@gmail.com>
2020-11-10 20:00:30 +0100

    Add exercise 02 using state of two properties

---
+++ Modulo-03-React/modulo-03-leccion-07-hooks/modulo-03-leccion-07-ejercicio-01-02/src/components/Button.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function Button() {
+  const [number, setNumber] = useState(28);
+  const [gift, setGift] = useState(1);
+
+  const getNumber = () => {
+    setNumber(number + 1);
+    setGift(gift + 1);
+  };
+
+  // const getGifts = () => {
+  //   setGift(gift + 1);
+  // };
+
+  return (
+    <>
+      <p>Hoy tengo {number} años de edad</p>
+      {/* <button type="button" onClick={getNumber}>
+        Hazme más viejo
+      </button> */}
+      <p>Tengo {gift} regalos</p>
+      <button type="button" onClick={getNumber}>
+        Hazme más viejo y dame regalos
+      </button>
+    </>
+  );
+}
+
+export default Button;
