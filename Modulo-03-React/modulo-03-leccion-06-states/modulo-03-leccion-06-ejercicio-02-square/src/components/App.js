96f03bf661cdecb203c3e668374dfe5cd414bd71
crisgarm <crisgarm9@gmail.com>
2020-11-11 11:29:04 +0100

    Add same exercise with functional component and hooks

--- Modulo-03-React/modulo-03-leccion-06-states/modulo-03-leccion-06-ejercicio-02-square/src/components/App.js
+++ Modulo-03-React/modulo-03-leccion-06-states/modulo-03-leccion-06-ejercicio-02-square/src/components/App.js
@@ -2,19 +2,12 @@ import React from "react";
 import "../stylesheets/App.css";
 import Square from "./Square";
 
-class App extends React.Component {
-  // eslint-disable-next-line
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Square cuadradro="blue" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Square />
+    </div>
+  );
 }
 
 export default App;
--- Modulo-03-React/modulo-03-leccion-06-states/modulo-03-leccion-06-ejercicio-02-square/src/components/Square.js
+++ Modulo-03-React/modulo-03-leccion-06-states/modulo-03-leccion-06-ejercicio-02-square/src/components/Square.js
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/App.css";
 
-class Square extends React.Component {
-  // eslint-disable-next-line
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { color: "blue" };
-  }
-  handleClick() {
-    this.setState((prevState) => {
+function Square() {
+  const [color, setColor] = useState("blue");
+
+  function handleClick() {
+    setColor((color) => {
       let newSquare;
-      if (prevState.color !== "blue") {
+      if (color !== "blue") {
         console.log("Entro en if");
         newSquare = "blue";
       } else {
         newSquare = "red";
         console.log("Entro en else");
       }
-      return { color: newSquare };
+      return newSquare;
     });
   }
-  render() {
-    return <div className={this.state.color} onClick={this.handleClick}></div>;
-  }
-}
 
+  return <div className={color} onClick={handleClick}></div>;
+}
 export default Square;
