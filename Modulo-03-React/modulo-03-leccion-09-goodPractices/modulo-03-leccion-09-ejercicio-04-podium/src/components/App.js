38aedf360bdce015cb82846b62d37313acb464a0
crisgarm <crisgarm9@gmail.com>
2020-11-15 20:26:50 +0100

    Add exercise 04 about array's destructuring

--- Modulo-03-React/modulo-03-leccion-09-goodPractices/modulo-03-leccion-09-ejercicio-04-podium/src/components/App.js
+++ Modulo-03-React/modulo-03-leccion-09-goodPractices/modulo-03-leccion-09-ejercicio-04-podium/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../stylesheets/App.css";
+import Winners from "./Winners";
 
 class App extends React.Component {
   // eslint-disable-next-line
@@ -8,7 +9,11 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="App">Hola Cris</div>;
+    return (
+      <div className="App">
+        <Winners />
+      </div>
+    );
   }
 }
 
