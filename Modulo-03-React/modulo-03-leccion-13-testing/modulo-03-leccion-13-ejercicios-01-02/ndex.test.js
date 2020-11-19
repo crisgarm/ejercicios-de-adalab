a8d1b70a41ac380e1f1621ff1cba121812bfd713
crisgarm <crisgarm9@gmail.com>
2020-11-19 20:18:42 +0100

    Add test suite

--- Modulo-03-React/modulo-03-leccion-13-testing/modulo-03-leccion-13-ejercicios-01-02/ndex.test.js
+++ Modulo-03-React/modulo-03-leccion-13-testing/modulo-03-leccion-13-ejercicios-01-02/ndex.test.js
@@ -1,5 +1,15 @@
 const sum = require("./index");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("Sum App", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test("adds -1 + 1 to equal 0", () => {
+    expect(sum(-1, 1)).toBe(0);
+  });
+
+  test("adds -1.2 + 2 to equal 0.80", () => {
+    expect(sum(-1.2, 2)).toBe(0.8);
+  });
 });
